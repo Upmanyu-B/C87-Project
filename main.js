5231b0714e93cd51f22aba31a52510abbb969d5a
Upmanyu-B <139064550+Upmanyu-B@users.noreply.github.com>
2024-06-14 08:38:27 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,36 @@
+ctx.beginPath;
+ctx.strokeStyle = "grea";
+ctx.lineWidth = 1;
+ctx.rect(x,y,width,height);
+ctx.stroke();
+
+ctx.beginPath;
+ctx.strokeStyle = "blue";
+ctx.lineWidth = 5;
+ctx.arc(x,y,r,startAngle, endAngle);
+ctx.strock();
+
+ctx.beginPath;
+ctx.strokeStyle = "black";
+ctx.lineWidth = 5;
+ctx.arc(x,y,r,startAngle, endAngle);
+ctx.strock();
+
+ctx.beginPath;
+ctx.strokeStyle = "red";
+ctx.lineWidth = 5;
+ctx.arc(x,y,r,startAngle, endAngle);
+ctx.strock();
+
+ctx.beginPath;
+ctx.strokeStyle = "orange";
+ctx.lineWidth = 5;
+ctx.arc(x,y,r,startAngle, endAngle);
+ctx.strock();
+
+ctx.beginPath;
+ctx.strokeStyle = "green";
+ctx.lineWidth = 5;
+ctx.arc(x,y,r,startAngle, endAngle);
+ctx.strock();
+
